3246806439495081e60108b9f50787435aa73ac0
leoazzi14 <110132974+leoazzi14@users.noreply.github.com>
2022-10-11 16:55:55 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -40,6 +40,34 @@ function escreva(){
      document.write("O resul2tado é" + resultado)
  }
 
+ function soma(){
+   let n1 = document.getElementById("n1").value;
+   let n2 = document.getElementById("n2").value;
+   let n3 = document.getElementById("n3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resultado").innerHTML = r;
+
+ }
+
+ function media(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = Number(n1) + Number(n2) + Number(n3) / 3;
+    document.getElementById("resultado").innerHTML = r;
+
+
+ }
+
+ function falta(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+     let r = 180 - Number(n1) + Number(n2) + Number(n3) ;
+     document.getElementById("resultado").innerHTML = r;
+
+ }
+
 
 
 
